db5e501ff7e89a892f67099bc3a10751d9691201
Zeltoria <123985221+Zeltoria@users.noreply.github.com>
2023-05-13 11:44:58 +0700

    Update main-menusearching.js

--- plugins/main-menusearching.js
+++ plugins/main-menusearching.js
@@ -44,7 +44,7 @@ ${readMore}
 ┆≼≽ whatanime
 ┆≼≽ whatmusic
 ❏–––––––––––––––༓`
-conn.sendButton(m.chat, tek, wm, thumb, [['Owner',`.owner`],['Sewa',`.sewa`]], fkontak)
+conn.sendMessage(m.chat, {image: {url: global.thumb}, caption: tek}, m)
 }
 
 handler.command = /^(menusearch)$/i
