678010eccdb4cc0e91d0f00415959cf5c912ec35
Zeltoria <123985221+Zeltoria@users.noreply.github.com>
2023-05-13 11:02:00 +0700

    Update main-menufun.js

--- plugins/main-menufun.js
+++ plugins/main-menufun.js
@@ -48,7 +48,7 @@ ${readMore}
 ┆≼≽ wibucek
 ┆≼≽ iqtest
 ❏–––––––––––––––༓`
-conn.sendButton(m.chat, tek, wm, thumb, [['Owner',`.owner`],['Sewa',`.sewa`]], fkontak)
+conn.sendMessage(m.chat, {image: {url: global.thumb}, caption: tek}, m)
 }
 handler.command = /^(menufun)$/i
 
