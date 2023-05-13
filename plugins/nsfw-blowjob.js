62a2f013642941032d101083ef591304acc3bcc5
Zeltoria <123985221+Zeltoria@users.noreply.github.com>
2023-05-13 13:27:30 +0700

    Update nsfw-blowjob.js

--- plugins/nsfw-blowjob.js
+++ plugins/nsfw-blowjob.js
@@ -1,8 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-  await m.reply(`*_ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ_*`)
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-           conn.sendMessage(m.chat, { image : { url : pickRandom(animeblowjob) }, caption : 'Cih dasar sangean' }, m)
+           conn.sendMessage(m.chat, { image : { url : pickRandom(animeblowjob) }, caption : `Cih Dasar Sangean` }, m)
 }
 handler.help = ['animeblowjob']
 handler.tags = ['premium', 'nsfw']
