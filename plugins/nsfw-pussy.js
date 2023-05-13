de0d140150b6e97a513a730ed0bba4a1af1536e8
MikoNia <109132477+Miko7799@users.noreply.github.com>
2023-05-13 13:59:30 +0700

    Update nsfw-pussy.js

--- plugins/nsfw-pussy.js
+++ plugins/nsfw-pussy.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   await m.reply(`*_ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ_*`)
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-  conn.sendButton(m.chat, `Nih *${name}*`, botdate, pickRandom(pussy), [['Next', `/pussy`]],m)
+              conn.sendMessage(m.chat, { image : { url : pickRandom(pussy) }, caption : `Cih Dasar Sangean` }, m)
 }
 handler.help = ['pussy']
 handler.tags = ['nsfw','premium']
