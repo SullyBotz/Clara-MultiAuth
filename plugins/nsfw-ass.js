4ff9974b863ae146fb5d6498393ac159f9476271
MikoNia <109132477+Miko7799@users.noreply.github.com>
2023-05-13 12:58:34 +0700

    Update nsfw-ass.js

--- plugins/nsfw-ass.js
+++ plugins/nsfw-ass.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-         conn.sendMessage(m.chat, { image : { url : animeass }, caption : 'Nih Kak Husbunya' }, m)
+         conn.sendMessage(m.chat, { image : { url : animeass }, caption : 'Cih dasar sangean' }, m)
 }
 handler.help = ['ass']
 handler.tags = ['nsfw','premium']
