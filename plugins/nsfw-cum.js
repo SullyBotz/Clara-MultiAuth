b75f458c3dc97720db0fe2d4d40ee82bb1790952
MikoNia <109132477+Miko7799@users.noreply.github.com>
2023-05-13 13:08:00 +0700

    Update nsfw-cum.js

--- plugins/nsfw-cum.js
+++ plugins/nsfw-cum.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-  conn.sendButton(m.chat, `Cih Dasar Sangean`, wm, pickRandom(animecum), [['\nJadi Sange', `huuu`]],m)
+           conn.sendMessage(m.chat, { image : { url : animecum }, caption : 'Cih dasar sangean' }, m)
 }
 handler.help = ['cum']
 handler.tags = ['nsfw','premium']
