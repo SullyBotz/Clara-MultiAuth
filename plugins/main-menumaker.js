465c9f710bd3724ab9ed76863d51fbf3ca3be6dd
Zeltoria <123985221+Zeltoria@users.noreply.github.com>
2023-05-13 11:04:41 +0700

    Update main-menumaker.js

--- plugins/main-menumaker.js
+++ plugins/main-menumaker.js
@@ -58,7 +58,7 @@ ${readMore}
 ┆≼≽ bonk
 ┆≼≽ hornycard
 ❏–––––––––––––––༓`
-conn.sendButton(m.chat, tek, wm, thumb, [['Owner',`.owner`],['Sewa',`.sewa`]], fkontak)
+conn.sendMessage(m.chat, {image: {url: global.thumb}, caption: tek}, m)
 }
 
 handler.command = /^(menumaker)$/i
