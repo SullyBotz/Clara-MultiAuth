bf9233fe05b523981dfc6274bff6f1b6e0d26416
MikoNia <109132477+Miko7799@users.noreply.github.com>
2023-05-13 13:48:32 +0700

    Update nsfw-foot.js

--- plugins/nsfw-foot.js
+++ plugins/nsfw-foot.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = conn.getName(who)
-             conn.sendMessage(m.chat, { image : { url : foot }, caption : 'Cih dasar sangean' }, m)
+                        conn.sendMessage(m.chat, { image : { url : pickRandom(foot) }, caption : `Cih Dasar Sangean` }, m)
 }
 handler.help = ['foot']
 handler.tags = ['nsfw','premium']
